b582644c463721495295bfae7e7b1b29e549a773
Stefano Paulo <stefanopaulo47@gmail.com>
2026-02-04 16:37:39 -0300

    OrderStatus enum

---
+++ src/main/java/dev/projetos/stefano/order/api/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package dev.projetos.stefano.order.api.enums;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private final int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalStateException("Invalid OrderStatus code");
+    }
+}
